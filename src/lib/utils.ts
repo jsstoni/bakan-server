52d37a0e28c5eff2391c404e075f64995a79dbb1
jsstoni <jsstoniha@gmail.com>
2024-09-28 19:58:51 -0300

    generador random

---
+++ src/lib/utils.ts
@@ -0,0 +1,9 @@
+export function generateToken(length: number) {
+  const charset =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let token = '';
+  while (token.length < length) {
+    token += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+  return token;
+}
