fc1739c55143b431bfef0d1d27c41ec7a101c056
jsstoni <jsstoniha@gmail.com>
2024-09-28 00:42:36 -0300

    api para registro y login

--- src/api/web.ts
+++ src/api/web.ts
@@ -1,7 +1,67 @@
+import { JWT_SECRET } from '@/lib/env';
+import prisma from '@/lib/prisma';
+import { validate_login, validate_register } from '@/lib/validate';
 import isAuthorized from '@/middleware/auth/authorized';
 import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
+
+router.post('/register', async (req, res, next) => {
+  try {
+    const parsedData = validate_register.parse(req.body);
+    const { name, email, password, repassword } = parsedData;
+
+    if (password !== repassword) {
+      throw new Error('Passwords do not match');
+    }
+
+    const users = await prisma.users.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+
+    res
+      .status(StatusCodes.CREATED)
+      .send({ name: users.name, email: users.email });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const parsedData = validate_login.parse(req.body);
+    const { email, password } = parsedData;
+
+    const user = await prisma.users.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const isPasswordCorrect = password === user.password;
+    const token = jwt.sign({ id: user.id }, JWT_SECRET);
+
+    res
+      .status(StatusCodes.OK)
+      .send({ name: user.name, email: user.email, token });
+
+    if (!isPasswordCorrect) {
+      throw new Error('Incorrect password');
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get('/admin', isAuthorized, (_, res) => {
   res.send('admin');
--- src/lib/validate.ts
+++ src/lib/validate.ts
@@ -1,9 +1,21 @@
 import { z } from 'zod';
 
+const emailSchema = z
+  .string({ message: 'Email is required' })
+  .email({ message: 'Email is invalid' });
+
+const passwordSchema = z
+  .string({ message: 'Password is required' })
+  .min(6, { message: 'Password must be at least 6 characters' });
+
 export const validate_register = z.object({
-  email: z
-    .string({ message: 'Email is required' })
-    .email({ message: 'Email is invalid' }),
-  password: z.string({ message: 'Password is required' }).min(6),
+  name: z.string({ message: 'Username is required' }),
+  email: emailSchema,
+  password: passwordSchema,
   repassword: z.string({ message: 'Repeat password is required' }).min(6),
+});
+
+export const validate_login = z.object({
+  email: emailSchema,
+  password: passwordSchema,
 });
