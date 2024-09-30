c7df81836961885ccee1a46b75a5f255f1bd1668
jsstoni <jsstoniha@gmail.com>
2024-09-30 14:50:38 -0300

    obteniendo snipper por id

--- src/api/snippet.ts
+++ src/api/snippet.ts
@@ -1,19 +1,19 @@
-import prisma from '@/lib/prisma';
-import { generateToken } from '@/lib/utils';
-import { validate_snippet } from '@/lib/validate';
-import isAuthorized from '@/middleware/auth/authorized';
-import { Request, Router } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import prisma from "@/lib/prisma";
+import { generateToken } from "@/lib/utils";
+import { validate_snippet } from "@/lib/validate";
+import isAuthorized from "@/middleware/auth/authorized";
+import { Request, Router } from "express";
+import { StatusCodes } from "http-status-codes";
 
 const router = Router();
 
-router.post('/', isAuthorized, async (req, res, next) => {
+router.post("/", isAuthorized, async (req, res, next) => {
   try {
     const parsedData = validate_snippet.parse(req.body);
     const { title, lang, code, token } = parsedData;
     let uuid = token;
     if (!req.token) {
-      throw new Error('Unauthorized: You are already logged in');
+      throw new Error("Unauthorized: You are already logged in");
     }
 
     if (!uuid) {
@@ -34,15 +34,37 @@ router.post('/', isAuthorized, async (req, res, next) => {
   }
 });
 
-router.get('/', isAuthorized, async (req, res, next) => {
+router.get("/", isAuthorized, async (req, res, next) => {
   try {
     if (!req.token) {
-      throw new Error('Unauthorized: You are already logged in');
+      throw new Error("Unauthorized: You are already logged in");
     }
     const userId = req.token.id;
 
     const snippets = await prisma.snippets.findMany({ where: { userId } });
     res.status(StatusCodes.OK).json(snippets);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", isAuthorized, async (req, res, next) => {
+  try {
+    if (!req.token) {
+      throw new Error("Unauthorized: You are already logged in");
+    }
+    const userId = req.token.id;
+    const uuid = req.params.id;
+
+    const snippet = await prisma.snippets.findUnique({
+      where: { userId, uuid },
+    });
+
+    if (!snippet) {
+      throw new Error(uuid);
+    }
+
+    res.status(StatusCodes.OK).json(snippet);
   } catch (error) {
     next(error);
   }
